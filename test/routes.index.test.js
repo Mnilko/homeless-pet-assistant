29db4800d593ac190c4b3b5116afbd3c0f940708
Mezentsev Valentin <mezentsev.valentin@gmail.com>
2019-01-23 17:30:45 +0200

    Add test step

--- test/routes.index.test.js
+++ test/routes.index.test.js
@@ -17,7 +17,7 @@ describe('routes : index', () => {
           res.status.should.eql(200);
           res.type.should.eql('application/json');
           res.body.status.should.equal('success');
-          res.body.message.should.eql('hello, world!');
+          res.body.message.should.eql('hello, from pet assistant!!!');
           done();
         });
     });
